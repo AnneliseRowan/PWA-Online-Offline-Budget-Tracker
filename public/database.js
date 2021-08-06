f989f7f26a9666e460feebad6e7356a838a2b523
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-08-06 13:57:17 -0400

    adding to database.js file, trying to build out indexDb, need to run to see if working

--- public/database.js
+++ public/database.js
@@ -7,41 +7,53 @@ request.onupgradeneeded = ({ target }) => {
   const db = target.result; 
 
   // Creates an object store with a listID keypath that can be used to query on.
-  const budgetStore = db.createdObjectStore("budgetDb", {keyPath: "listID"});
-  // Creates a statusIndex that we can query on.
-  budgetStore.createIndex("statusIndex", "status")
+  db.createdObjectStore("waiting", { keyPath: "listID", autoIncrement: true });
 }; 
 
 // Opens a transaction, accesses the toDoList objectStore and statusIndex.
-request.onsuccess = event => {
-  const db = event.target.result; 
-  const transaction = db.transaction(["budgetDb"], "readwrite");
-  const budgetStore = transaction.objectStore("budgetDb"); 
-  const statusIndex = budgetStore.index("statusIndex"); 
+request.onsuccess = ({ target }) => {
+  const db = target.result; 
 
-  // Adds data to our objectStore??????
-
-  // Return an item by keyPath
-  const getRequest = budgetStore.get("1");
-  getRequest.onsuccess = () => {
-    console.log(getRequest.result); 
+  if (navigator.onLine) {
+      checkDatabase();
   }
+};
 
-  // Return an item by index
-  const getRequestIndex = statusIndex.getAll("complete");
-  getRequestIndex.onsuccess= () => {
-    console.log(getRequestIndex.result);
-  };
+request.onerror = ({ target }) => {
+    console.log(`Uhhh-Ohhh ${target}`)
+}
 
-  // Opens a Cursor request and iterates over the documents -- Don't know if I need this
-  // const getCursorRequest = toDoListStore.openCursor();
-  //   getCursorRequest.onsuccess = e => {
-  //     const cursor = e.target.result;
-  //     if (cursor) {
-  //       console.log(cursor.value);
-  //       cursor.continue();
-  //     } else {
-  //       console.log("No documents left!");
-  //     }
-  //   };
-}; 
+const saveInput = (input) => {
+
+}
+
+const checkInput = (input) => {
+
+}
+
+getAll.onsuccess = () => {
+    const transaction = db.transaction(["waiting"], "readwrite"); 
+    const store = transaction.objectStore("pending"); 
+    const getAll = store.getAll(); 
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "appication/json",
+                },
+            })
+            .then(response => response.json())
+            .then(() => {
+                transaction; 
+                store; 
+                store.clear(); 
+            });
+        }
+    };
+}
+
+window.addEventListener("online", checkDatabase); 
